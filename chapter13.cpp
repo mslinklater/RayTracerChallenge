afdd73cdb6678288082f0926c4ab841cfb12995d
Martin Linklater <mslinklater@gmail.com>
2026-04-19 11:14:09 +0100

    tweaked 13 picture

--- chapter13.cpp
+++ chapter13.cpp
@@ -16,20 +16,20 @@ int main()
 {
     std::cout << "Chapter 13 challenge !!!" << std::endl;
 
-    Camera camera(kCanvasSize, kCanvasSize, M_PI / 3.0f);
+    Camera camera(kCanvasSize * (16.f / 9.f), kCanvasSize, M_PI / 3.0f);
     camera.SetTransform(
         Matrix::ViewTransform(Point(2.0f, 5.0f, -5.0f), Point(0.0f, 1.0f, 0.0f), Vector(0.0f, 1.0f, 0.0f)));
 
     World world;
 
     // Light
-    Light lightr(Point(-10.0f, 10.0f, -10.0f), Color(1.0f, 1.0f, 0.0f));
+    Light lightr(Point(-10.0f, 10.0f, -10.0f), Color(1.0f, 1.0f, 1.0f));
     world.AddLight(lightr);
     // Light 2
-    Light lightg(Point(10.0f, 10.0f, -10.0f), Color(0.0f, 1.0f, 1.0f));
+    Light lightg(Point(10.0f, 10.0f, -10.0f), Color(0.8f, 0.8f, 0.8f));
     world.AddLight(lightg);
     // Light 2
-    Light lightb(Point(10.0f, 20.0f, 10.0f), Color(1.0f, 0.0f, 1.0f));
+    Light lightb(Point(10.0f, 20.0f, 10.0f), Color(0.5f, 0.5f, 0.5f));
     world.AddLight(lightb);
 
     // Floor
@@ -43,9 +43,11 @@ int main()
     // cylinder
     {
         Cylinder cylinder("cylinder1");
+        cylinder.SetMinimum(-1.f);
+        cylinder.SetMaximum(1.f);
         cylinder.SetTransform(Matrix::CreateTranslation(0.f, 1.0f, 0.f) * Matrix::CreateRotationX(0.f));
         Material mat;
-        mat.SetColor(Color(1.0f, 0.7f, 0.7f)).SetDiffuse(0.7f).SetSpecular(0.3f);
+        mat.SetColor(Color(1.0f, 1.0f, 1.0f)).SetDiffuse(0.2f).SetSpecular(0.3f);
         mat.SetReflective(0.5f);
         cylinder.SetMaterial(mat);
         world.AddObject(cylinder);
@@ -54,8 +56,10 @@ int main()
     {
         Cylinder cylinder("cylinder2");
         cylinder.SetTransform(Matrix::CreateTranslation(2.f, 1.0f, 0.f) * Matrix::CreateRotationX(1.f));
+        cylinder.SetMinimum(-1.f);
+        cylinder.SetMaximum(1.f);
         Material mat;
-        mat.SetColor(Color(0.7f, 1.0f, 0.7f)).SetDiffuse(0.7f).SetSpecular(0.3f);
+        mat.SetColor(Color(1.0f, 1.0f, 1.0f)).SetDiffuse(0.2f).SetSpecular(0.3f);
         mat.SetReflective(0.5f);
         cylinder.SetMaterial(mat);
         world.AddObject(cylinder);
@@ -64,8 +68,10 @@ int main()
     {
         Cylinder cylinder("cylinder3");
         cylinder.SetTransform(Matrix::CreateTranslation(-2.f, 1.0f, 0.f) * Matrix::CreateRotationX(2.f));
+        cylinder.SetMinimum(-1.f);
+        cylinder.SetMaximum(1.f);
         Material mat;
-        mat.SetColor(Color(0.7f, 0.7f, 1.0f)).SetDiffuse(0.7f).SetSpecular(0.3f);
+        mat.SetColor(Color(1.0f, 1.0f, 1.0f)).SetDiffuse(0.2f).SetSpecular(0.3f);
         mat.SetReflective(0.5f);
         cylinder.SetMaterial(mat);
         world.AddObject(cylinder);
